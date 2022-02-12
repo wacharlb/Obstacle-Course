bc7638d92ef64f8da0d55fc4bce23076261a3cef
Charles Beall <wacharlb@gmail.com>
2022-02-12 01:49:08 -0800

    Added Player, ground, walls and mover script to control player movement.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0f, zValue);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game 1");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
     }
 }
